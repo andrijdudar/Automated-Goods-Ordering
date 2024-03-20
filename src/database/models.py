9c4b29ef80d4c0ad98f8465a9cffd81a46579559
Evgenius87 <plotnikov.evgenij@ukr.net>
2024-03-20 14:20:28 +0200

    update models

--- src/database/models.py
+++ src/database/models.py
@@ -3,7 +3,7 @@ from typing import List
 from enum import Enum
 import enum
 
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, func, Table, Enum
+from sqlalchemy import Column, Integer,Float, String, Boolean, DateTime, func, Table, Enum
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.sql.sqltypes import DateTime
@@ -13,13 +13,40 @@ Base = declarative_base()
 
 
 
-image_m2m_tag = Table(
-    "note_m2m_tag",
+dish_m2m_tag = Table(
+    "dish_m2m_tag",
     Base.metadata,
     Column("id", Integer, primary_key=True),
     Column("dish_id", Integer, ForeignKey("dishes.id", ondelete="CASCADE")),
     Column("tag_id", Integer, ForeignKey("tags.id", ondelete="CASCADE")),
 )
+
+dish_m2m_ingredient = Table(
+    'dish_m2m_ingredient', 
+    Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column('dish_id', Integer, ForeignKey('dishes.id', ondelete="CASCADE")),
+    Column('ingredient_id', Integer, ForeignKey('ingredients.id', ondelete="CASCADE")),
+    Column('quantity', Float) 
+                          )
+
+dish_m2m_premix = Table(
+    'dish_m2m_premix', 
+    Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column('dish_id', Integer, ForeignKey('dishes.id', ondelete="CASCADE")),
+    Column('premix_id', Integer, ForeignKey('premixes.id', ondelete="CASCADE")),
+    Column('quantity', Float) 
+                          )
+
+premix_m2m_ingredient = Table(
+    'premix_m2m_ingredient', 
+    Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column('premix_id', Integer, ForeignKey('premixes.id', ondelete="CASCADE")),
+    Column('ingredient_id', Integer, ForeignKey('ingredients.id', ondelete="CASCADE")),
+    Column('quantity', Float) 
+                          )
 
 
 class Dish(Base):
@@ -29,15 +56,39 @@ class Dish(Base):
     image_public_id = Column(String(255))
     dish_name = Column(String(200), unique=True)
     description = Column(String(900))
-    ingredients = Column(String(900))
+    ingredients =  relationship("Ingredient", secondary=dish_m2m_ingredient, back_populates="dishes")
     user_id = Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
-    tags = relationship("Tag", secondary=image_m2m_tag, back_populates="dishes")
+    tags = relationship("Tag", secondary=dish_m2m_tag, back_populates="dishes")
     stop_list = Column(Boolean)
     price = Column(Integer)
     created_at = Column("created_at", DateTime, default=func.now())
     updated_at = Column("updated_at", DateTime, default=func.now(), onupdate=func.now())
     category_id = Column(Integer, ForeignKey('categories.id'))
     category = relationship('Category', back_populates='dishes')
+
+
+class Ingredient(Base):
+    __tablename__ = "ingredients"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200), unique=True)
+    product = Column(String(200), unique=True)
+    amount = Column(Integer)
+    suma = Column(Integer)
+    dishes = relationship("Dish", secondary=dish_m2m_ingredient, back_populates="ingredients")
+    provider = Column(Integer, ForeignKey('providers.id'))
+    created_at = Column("created_at", DateTime, default=func.now())
+    updated_at = Column("updated_at", DateTime, default=func.now(), onupdate=func.now())
+
+
+class Premix(Base):
+    __tablename__ = "premixes"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200), unique=True)
+    dishes = relationship("Dish", secondary=dish_m2m_premix, back_populates="premixes")
+    ingredients = relationship("Ingredient", secondary=premix_m2m_ingredient, back_populates="premixes")
+    created_at = Column("created_at", DateTime, default=func.now())
+    updated_at = Column("updated_at", DateTime, default=func.now(), onupdate=func.now())
+
 
 
 class Category(Base):
@@ -55,7 +106,7 @@ class Tag(Base):
     __tablename__ = "tags"
     id = Column(Integer, primary_key=True)
     name_tag = Column(String(25), nullable=False, unique=True)
-    dishes  = relationship("Dish", secondary=image_m2m_tag, back_populates="tags")
+    dishes  = relationship("Dish", secondary=dish_m2m_tag, back_populates="tags")
 
 
 
@@ -81,6 +132,15 @@ class User(Base):
     role = Column('role', Enum(Role), default=Role.user)
     bot_role = Column(String(15))
     information = Column(String, nullable=True)
+
+
+class Provider(Base):
+    __tablename__ = "providers"
+    id = Column(Integer, primary_key=True)
+    provider_name = Column(String(200), nullable=True)
+    salesman_name = Column(String(200), nullable=True)
+    salesman_phone = Column(Integer)
+
 
 
 
