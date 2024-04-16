1f051f6939cac76c29901e275cc3d3c0cd042ede
Andrii Dudar <andrij.dudar@gmail.com>
2024-04-17 00:34:19 +0300

    Update App.js to include a greeting message in the React app

--- front_end_automated-goods-ordering/src/App.js
+++ front_end_automated-goods-ordering/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Evgeniy! I am a React app. I am a part of the automated goods ordering system.
         </p>
         <a
           className="App-link"
